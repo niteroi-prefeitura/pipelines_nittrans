3b067d5375f22866f167a2ac408e48d240b73202
Thiago Koch <tkoch.dev@gmail.com>
2025-02-10 11:14:39 -0300

    refactor: cria um arquivo único para a função de enviar email de erro e remove comentários desnecessários do código do waze features traffic

--- src/grouplayer_waze_features_traffic.py
+++ src/grouplayer_waze_features_traffic.py
@@ -3,26 +3,23 @@ import os
 import requests
 import pandas as pd
 from registers import clear_old_registers, add_registers
-from datetime import datetime, timedelta
+from send_email import send_email_error
+from datetime import datetime
 from arcgis.gis import GIS
 from smtplib import SMTP
-from email.mime.text import MIMEText
 
 load_dotenv()
 
-SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
-SENDER_EMAIL_PASSWORD = os.getenv("SENDER_EMAIL_PASSWORD")
-RECIPIENT_EMAIL_ADDRESS = os.getenv("RECIPIENT_EMAIL_ADDRESS")
+AGOL_USERNAME = os.getenv("AGOL_USERNAME")
+AGOL_PASSWORD = os.getenv("AGOL_PASSWORD")
 LAYER_ID = os.getenv("LAYER_ID")
 
-# Função para obter dados da API
 def get_api_data(url):
 
     response = requests.get(url)
     response.raise_for_status()
     return response.json()
 
-# Função para processar os dados e criar o DataFrame
 def process_data(resposta_json):
 
     df = pd.DataFrame(resposta_json['jams'])
@@ -34,35 +31,13 @@ def process_data(resposta_json):
 
         now = datetime.now()
         df['Data'] = now.strftime(format='%d/%m/%Y %H:%M')
-        df['Data'] = pd.to_datetime(df['Data'], dayfirst=True) #+ timedelta(hours=3)
+        df['Data'] = pd.to_datetime(df['Data'], dayfirst=True)
         df['Data'] = df['Data'].apply(lambda x: int(x.timestamp() * 1000))
 
     return df
 
-# Função para enviar e-mail com o log de erros
-def send_email(log_erros):
-    try:
-        if not log_erros:
-            return
-        email_body = "<br>".join(f"<p>{erro}</p>" for erro in log_erros)
-        msg = MIMEText(email_body, 'html')
-        msg['Subject'] = "Erro_Script_Live_Waze_Traffic"
-        msg['From'] = SENDER_EMAIL_ADDRESS
-        msg['To'] = RECIPIENT_EMAIL_ADDRESS
-        
-        with SMTP("smtp.gmail.com", 587) as smtp:
-            smtp.starttls()
-            smtp.login(SENDER_EMAIL_ADDRESS, SENDER_EMAIL_PASSWORD)
-            smtp.sendmail(msg['From'], msg['To'], msg.as_string())
-    except Exception as e:
-        print(f"Erro ao enviar e-mail: {e}")
-
-# Função principal
 def main():
     log_erros = []
-    
-    AGOL_USERNAME = os.getenv("AGOL_USERNAME")
-    AGOL_PASSWORD = os.getenv("AGOL_PASSWORD")
     
     url = "https://www.waze.com/row-partnerhub-api/partners/18863635230/waze-feeds/feef83ff-1b4f-4d93-9d7a-dd3ea11304d3?format=1"
     
@@ -70,13 +45,13 @@ def main():
     
     if isinstance(json_response, str):
         log_erros.append(json_response)
-        send_email(log_erros)
+        send_email_error(log_erros)
         return
     
     df = process_data(json_response)
     if isinstance(df, str):
         log_erros.append(df)
-        send_email(log_erros)
+        send_email_error(log_erros)
         return
     
     try:
@@ -87,7 +62,7 @@ def main():
         
     except Exception as e:
         log_erros.append(f"Erro ao conectar no ArcGIS: {e}")
-        send_email(log_erros)
+        send_email_error(log_erros)
         return
     
     registers_removed = clear_old_registers(traffic_agol)
@@ -98,7 +73,7 @@ def main():
     if isinstance(registers_added, str):
         log_erros.append(registers_added)
     
-    send_email(log_erros)
+    send_email_error(log_erros)
 
 if __name__ == "__main__":
     main()
