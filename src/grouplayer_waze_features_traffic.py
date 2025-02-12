03f23e61148e36e094860edde61d74ef76b6d5c6
tkoch97 <tkoch.dev@gmail.com>
2025-02-12 11:42:32 -0300

    refactor: coloca a url do waze partner hub api no arquivo .env

--- src/grouplayer_waze_features_traffic.py
+++ src/grouplayer_waze_features_traffic.py
@@ -11,6 +11,7 @@ load_dotenv()
 AGOL_USERNAME = os.getenv("AGOL_USERNAME")
 AGOL_PASSWORD = os.getenv("AGOL_PASSWORD")
 LAYER_ID = os.getenv("LAYER_ID")
+WAZE_PARTNER_HUB_API_URL = os.getenv("WAZE_PARTNER_HUB_API_URL")
 
 def update_layers_on_arcgis(traffic_layer, df):
     errors = []
@@ -26,11 +27,10 @@ def update_layers_on_arcgis(traffic_layer, df):
     return errors
 
 def main():
-    api_url = "https://www.waze.com/row-partnerhub-api/partners/18863635230/waze-feeds/feef83ff-1b4f-4d93-9d7a-dd3ea11304d3?format=1"
     errors = []
     
     try:
-        json_response = get_api_data_as_json(api_url)
+        json_response = get_api_data_as_json(WAZE_PARTNER_HUB_API_URL)
 
         df = process_data(json_response)
 
