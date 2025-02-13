472ac08a659443ded6d2a3ec04dd2fba567f5955
tkoch97 <tkoch.dev@gmail.com>
2025-02-13 13:14:17 -0300

    refactor: refatora a função generate_token e adiciona novas variáveis de ambiente

---
+++ src/generate_token.py
@@ -0,0 +1,20 @@
+import requests
+from typing import Dict
+
+
+def generate_portal_token(**credentials: Dict[str, str]):
+
+    params = {
+        "username": credentials["portal_username"],
+        "password": credentials["portal_password"],
+        "referer": credentials["portal_referer"],
+        "f": "json",
+    }
+
+    response = requests.post(credentials["url_to_generate_token"], data=params)
+
+    if response.status_code == 200:
+        token_info = response.json()
+        print(f"Token gerado: {token_info['token']}")
+    else:
+        print(f"Erro ao gerar token: {response.text}")
