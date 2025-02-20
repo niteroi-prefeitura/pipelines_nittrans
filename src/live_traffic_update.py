958e3a1cc19ea574c89357bf8614c2da0a7cb3a2
tkoch97 <tkoch.dev@gmail.com>
2025-02-20 09:20:03 -0300

    refactor: renomeia o arquivo update_layers_on_arcgis para replace_layers_on_arcgis, como também sua função interna para replace_polyline_layers_on_arcgis. Ajusta as importações de funções dentro do arquivo live_traffic_update e também cria o dicionário attribute_map

--- src/live_traffic_update.py
+++ src/live_traffic_update.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
-from utils.process_data import process_data
+from utils.process_data import process_data_live_traffic
 from utils.send_email import send_email_error
 from utils.get_api_data import get_api_data_as_json
 from utils.get_layer_on_arcgis import get_a_layer_index
-from utils.update_layers_on_arcgis import update_layers_on_arcgis
+from utils.replace_layers_on_arcgis import replace_polyline_layers_on_arcgis
 
 load_dotenv()
 
@@ -20,6 +20,22 @@ credentials_to_get_layer_on_arcgis = {
     "layer_id": os.getenv("LAYER_ID"),
 }
 
+attribute_map = {
+    'Pais': 'Pais',
+    'uuid': 'uuid',
+    'street': 'street',
+    'Final': 'endNode',
+    'Tipo_da_rua': 'Tipo_da_rua',
+    'Comprimento': 'length',
+    'Vel_km_h': 'Vel_km_h',
+    'Cidade': 'Cidade',
+    'Level_': 'level',
+    'delay': 'delay',
+    'speed': 'speed',
+    'turnType': 'turnType',
+    'Data': 'Data'
+}
+
 WAZE_PARTNER_HUB_API_URL = os.getenv("WAZE_PARTNER_HUB_API_URL")
 
 
@@ -29,12 +45,13 @@ def main():
     try:
         json_response = get_api_data_as_json(WAZE_PARTNER_HUB_API_URL)
 
-        df = process_data(json_response)
+        df = process_data_live_traffic(json_response)
 
         traffic_layer = get_a_layer_index(
             credentials_to_get_layer_on_arcgis, 2)
 
-        errors += update_layers_on_arcgis(traffic_layer, df)
+        errors += replace_polyline_layers_on_arcgis(
+            traffic_layer, df, attribute_map)
 
     except Exception as e:
         errors.append(str(e))
---
+++ src/utils/replace_layers_on_arcgis.py
@@ -0,0 +1,16 @@
+from registers import clear_all_registers_on_agol_layer, add_polyline_registers_on_agol_layer
+
+
+def replace_polyline_layers_on_arcgis(agol_layer, df, attribute_map: dict[str, str]):
+    errors = []
+
+    registers_removed = clear_all_registers_on_agol_layer(agol_layer)
+    if isinstance(registers_removed, str):
+        errors.append(registers_removed)
+
+    registers_added = add_polyline_registers_on_agol_layer(
+        df, attribute_map, agol_layer)
+    if isinstance(registers_added, str):
+        errors.append(registers_added)
+
+    return errors
