0169c0bd28de86e3b0cbf2311a9b415ded5b5c17
tkoch97 <tkoch.dev@gmail.com>
2025-03-09 22:40:43 -0300

    refactor: desacopla a função de filtrar o dataframe por tipo e aciddente

--- src/hist_accident_update.py
+++ src/hist_accident_update.py
@@ -1,4 +1,5 @@
 import os
+import pandas as pd
 from dotenv import load_dotenv
 from utils.send_email import send_email_error
 from utils.generate_token import generate_portal_token
@@ -6,6 +7,7 @@ from utils.get_layer_on_arcgis import get_a_layer_object_agol, get_atributes_of_
 from utils.process_data import process_data_waze_alerts
 from utils.get_api_data import get_api_data_as_json
 from utils.compare_attributes import compare_attributes
+from utils.filter_waze_alerts import filter_waze_alerts_by_alert_type
 from utils.update_layers_on_portal import update_point_layers_on_portal
 
 load_dotenv()
@@ -22,7 +24,7 @@ credentials_to_send_error_email = {
     "recipient_email_address": os.getenv('RECIPIENT_EMAIL_ADDRESS'),
 }
 
-credentials_to_get_layer_on_arcgis = {
+credentials_to_get_layer_on_agol = {
     "agol_username": os.getenv("AGOL_USERNAME"),
     "agol_password": os.getenv("AGOL_PASSWORD"),
     "layer_id": os.getenv("LAYER_ID"),
@@ -48,23 +50,46 @@ def main():
         # Trazer dataframe tratado sem filtro baseado na API do Waze
         df_alerts = process_data_waze_alerts(data)
 
+        df_accidents = filter_waze_alerts_by_alert_type(df_alerts, 'ACCIDENT')
+
         # Buscar layer de alertas no agol
         alerts_layer = get_a_layer_object_agol(
-            credentials_to_get_layer_on_arcgis, 1)
+            credentials_to_get_layer_on_agol, 1)
 
         # Criar função para pegar os atributos da alerts_layer
         alerts_layer_attributes = get_atributes_of_layer_object(
-            alerts_layer, "uuid")
+            alerts_layer, "uuid, Tipo")
 
         # comparar os atributos do alerts_layer com os atributos do df_tratado
         attributes_comparison = compare_attributes(
-            df_alerts, "tx_uuid", alerts_layer_attributes, "uuid")
+            df_accidents, "tx_uuid", alerts_layer_attributes, "uuid")
 
         print("Atributos iguais: ",
               attributes_comparison["matching_attributes"])
         print("Atributos apenas no df: ", attributes_comparison["only_in_df"])
         print("Atributos apenas na layer: ",
               attributes_comparison["only_in_layer"])
+
+        accidents_only_in_df = df_accidents[df_accidents['tx_uuid'].isin(
+            attributes_comparison["only_in_df"])]
+
+        matching_accidents = df_accidents[df_accidents['tx_uuid'].isin(
+            attributes_comparison["matching_attributes"])]
+
+        alerts_layer_attributes = pd.DataFrame(alerts_layer_attributes)
+
+        accidents_only_in_layer = alerts_layer_attributes[
+            (alerts_layer_attributes['uuid'].isin(
+                attributes_comparison["only_in_layer"]))
+            & (alerts_layer_attributes['Tipo'] == 'Trânsito parado')
+        ]
+
+        # accidents_only_in_layer = alerts_layer_attributes[alerts_layer_attributes['uuid'].isin(
+        #     attributes_comparison["only_in_layer"])]
+
+        print("Acidentes no DataFrame e na Layer: ", matching_accidents)
+        print("Acidentes apenas no DataFrame : ", accidents_only_in_df)
+        print("Acidentes apenas na Layer: ", accidents_only_in_layer)
 
         # Se os atributos forem iguais, atualizar apneas o campo de data na layer
 
--- src/utils/compare_attributes.py
+++ src/utils/compare_attributes.py
@@ -2,9 +2,6 @@ def compare_attributes(df, df_attribute, layer, layer_attribute):
     df_attributes = set(df[df_attribute])
     layer_attributes = {obj[layer_attribute] for obj in layer}
 
-    print(f'UUIDs no DataFrame: {df_attributes}')
-    print(f'UUIDs na camada: {layer_attributes}')
-
     matching_attributes = df_attributes.intersection(layer_attributes)
     only_in_df = df_attributes - layer_attributes
     only_in_layer = layer_attributes - df_attributes
