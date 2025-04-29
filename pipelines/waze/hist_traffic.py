28c61791d7816ebcaf7bebae290d1f624fa0747c
Admin SIGeo <168854865+sigeo-admin@users.noreply.github.com>
2025-04-29 15:39:18 -0300

    Development (#6)
    * docs: atualiza readme
    
    * docs: atualiza readme
    
    * chore: muda a url de acesso à api do waze de uma variable para um secret do prefect
    
    ---------
    
    Co-authored-by: tkoch97 <tkoch.dev@gmail.com>

--- pipelines/waze/hist_traffic.py
+++ pipelines/waze/hist_traffic.py
@@ -13,31 +13,35 @@ his_layers_url = Variable.get("waze_hist_portal_layers")
 user_agol = Secret.load("usuario-integrador-agol").get()
 user_portal = Secret.load("usuario-pmngeo-portal").get()
 
-URL_WAZE_API = os.getenv("WAZE_PARTNER_HUB_API_URL") or Variable.get("url_waze_api")["URL"]
-URL_TRAFFIC_HIST_PORTAL = os.getenv("URL_TRAFFIC_HIST_PORTAL") or his_layers_url["URL_TRAFEGO"]
-URL_TO_GENERATE_TOKEN = os.getenv("URL_TO_GENERATE_TOKEN") or gis_variables["URL_TO_GENERATE_TOKEN"]
-URL_GIS_ENTERPRISE = os.getenv("URL_GIS_ENTERPRISE") or gis_variables["URL_GIS_ENTERPRISE"]
+URL_WAZE_API = os.getenv("WAZE_PARTNER_HUB_API_URL") or Secret.load(
+    "waze_url_autenticada_api")
+URL_TRAFFIC_HIST_PORTAL = os.getenv(
+    "URL_TRAFFIC_HIST_PORTAL") or his_layers_url["URL_TRAFEGO"]
+URL_TO_GENERATE_TOKEN = os.getenv(
+    "URL_TO_GENERATE_TOKEN") or gis_variables["URL_TO_GENERATE_TOKEN"]
+URL_GIS_ENTERPRISE = os.getenv(
+    "URL_GIS_ENTERPRISE") or gis_variables["URL_GIS_ENTERPRISE"]
 
 CREDENTIALS_AGOL = {
     "agol_username": os.getenv("AGOL_USERNAME") or user_agol["username"],
-    "agol_password": os.getenv("AGOL_PASSWORD") or user_agol["password"],   
+    "agol_password": os.getenv("AGOL_PASSWORD") or user_agol["password"],
 }
 
 CREDENTIALS_PORTAL = {
     "username": os.getenv("PORTAL_USERNAME") or user_portal["username"],
-    "password": os.getenv("PORTAL_PASSWORD") or user_portal["password"],   
+    "password": os.getenv("PORTAL_PASSWORD") or user_portal["password"],
 }
 
 
-
-@flow(name="Histórico de Tráfego",log_prints=True)
+@flow(name="Histórico de Tráfego", log_prints=True)
 def waze_traffic_hist():
     try:
         waze_data = get_api_data_as_json(URL_WAZE_API)
 
         parsed_data = parse_traffic_hist_data(waze_data)
 
-        filtered_df = parsed_data[parsed_data["li_nivel"] >= 4] if not parsed_data.empty else parsed_data
+        filtered_df = parsed_data[parsed_data["li_nivel"]
+                                  >= 4] if not parsed_data.empty else parsed_data
 
         if not filtered_df.empty:
 
@@ -49,16 +53,17 @@ def waze_traffic_hist():
                 hist_feats.append({
                     "attributes": att
                 })
-            
-            token = generate_portal_token(CREDENTIALS_PORTAL, URL_GIS_ENTERPRISE, URL_TO_GENERATE_TOKEN)
 
-            portal_layer = get_layer_on_portal(URL_TRAFFIC_HIST_PORTAL, token, URL_GIS_ENTERPRISE) 
+            token = generate_portal_token(
+                CREDENTIALS_PORTAL, URL_GIS_ENTERPRISE, URL_TO_GENERATE_TOKEN)
 
-            create_new_feature(hist_feats,portal_layer)
+            portal_layer = get_layer_on_portal(
+                URL_TRAFFIC_HIST_PORTAL, token, URL_GIS_ENTERPRISE)
+
+            create_new_feature(hist_feats, portal_layer)
         else:
             print('Sem registros a serem adicionados')
-        
-       
+
     except Exception as e:
         error_message = str(e)
         raise ValueError(f"Erro durante a execução: {error_message}")
